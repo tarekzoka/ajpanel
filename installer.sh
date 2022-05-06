c1c9b9cfa035f8e8fd44d6d41afe0b9c0e7e8de5
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 01:41:06 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.ipk?raw=true
+opkg install --force-overwrite  https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.0.1_all.ipk?raw=true
 wait
 #!/bin/sh
 #
