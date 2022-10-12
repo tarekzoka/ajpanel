5af2e6d973fa59e7b951cde1c2933b5ca851fa8c
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-13 01:41:22 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,12 +6,12 @@
 ###########################################
 #!/bin/sh
 echo
-opkg install --force-overwrite https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v7.2.0_all.ipk?raw=true
+opkg install --force-overwrite https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v7.3.0_all.ipk?raw=true
 wait
 #!/bin/sh
 #
 
-wget -O /tmp/enigma2-plugin-extensions-ajpanel_v7.2.0_all.deb "https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v7.2.0_all.deb?raw=true"
+wget -O /tmp/enigma2-plugin-extensions-ajpanel_v7.3.0_all.deb "https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v7.2.0_all.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
