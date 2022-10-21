d18e2d8ee849a1c3fae6fe6f78149753d129230f
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-10-22 00:44:52 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@
 ###########################################
 #!/bin/sh
 echo
-opkg install --force-overwrite https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v7.3.0_all.ipk?raw=true
+opkg install --force-overwrite https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v7.4.0_all.ipk?raw=true
 wait
 #!/bin/sh
 #
