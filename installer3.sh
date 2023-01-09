e2141210d45f72a679bf75f315a3b189aa0b3ab4
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2023-01-09 23:05:09 +0200

    Update installer3.sh

--- installer3.sh
+++ installer3.sh
@@ -3,12 +3,12 @@
 #wget -q "--no-check-certificate" https://raw.githubusercontent.com/tarekzoka/ajpanel/main/installer3.sh -O - | /bin/sh
 
 ##########################################
-version=8.2.0
+version=8.4.0
 #############################################################
 TEMPATH=/tmp
 OPKGINSTALL="opkg install --force-overwrite"
-MY_IPK="enigma2-plugin-extensions-ajpanel_v8.2.0_all.ipk"
-MY_DEB="enigma2-plugin-extensions-ajpanel_v8.2.0_all.deb"
+MY_IPK="enigma2-plugin-extensions-ajpanel_v8.4.0_all.ipk"
+MY_DEB="enigma2-plugin-extensions-ajpanel_v8.4.0_all.deb"
 MY_URL="https://raw.githubusercontent.com/tarekzoka/ajpanel/main"
 # remove old version #
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AJPan
