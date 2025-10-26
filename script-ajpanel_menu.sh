2303f968ecae034ae6f62d268a3220f748369b79
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-10-26 04:38:40 +0300

    Add files via upload

--- script-ajpanel_menu.sh
+++ script-ajpanel_menu.sh
@@ -1,4 +1,9 @@
-wget "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu.tar.gz"
+#!/bin/bash
+#
+# Command: wget -q "--no-check-certificate" https://raw.githubusercontent.com/tarekzoka/ajpanel/refs/heads/main/script-ajpanel_menu.sh -O - | /bin/sh #
+echo "------------------------------------------------------------------------"
+echo "                     Installing ajpanel_menu"
+echo "------------------------------------------------------------------------" "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu.tar.gz"
 
 tar -xzf ajpanel_menu.tar.gz  -C /
 
