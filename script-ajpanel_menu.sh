66d49c8311a3900899936bf7809f2272c62f0806
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-10-26 03:45:20 +0300

    Add files via upload

---
+++ script-ajpanel_menu.sh
@@ -0,0 +1,13 @@
+wget "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu.tar.gz"
+
+tar -xzf ajpanel_menu.tar.gz  -C /
+
+wait
+rm -f /tmp/ajpanel_menu.tar.gz
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo BY-TAREK-HANFY   "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0 
