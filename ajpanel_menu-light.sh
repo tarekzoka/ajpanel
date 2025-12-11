9a1328ccdfc94dcda59e3be7a185aecbbdf1490c
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-12-11 15:21:38 +0200

    Add files via upload

--- ajpanel_menu-light.sh
+++ ajpanel_menu-light.sh
@@ -3,6 +3,8 @@
 
 wget -O /media/hdd/AJPanel_Backup/ajpanel_menu_Tarek.xml "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu_Tarek.xml"
 wait
+wget -O /media/sda1/AJPanel_Backup/ajpanel_menu_Tarek.xml "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu_Tarek.xml"
+wait
 wget -O /media/dreambox-storage/AJPanel_Backup/ajpanel_menu_Tarek.xml "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu_Tarek.xml"
 wait
 wget -O /media/usb/AJPanel_Backup/ajpanel_menu_Tarek.xml "https://raw.githubusercontent.com/tarekzoka/ajpanel/main/ajpanel_menu_Tarek.xml"
