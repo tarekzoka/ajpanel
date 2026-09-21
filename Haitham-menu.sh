8105db5fc065f66523179339e1ab2bf9b2d89950
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2026-09-21 19:30:56 +0300

    Add files via upload

---
+++ Haitham-menu.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+wget -O /media/hdd/AJPanel_Backup/ajpanel_menu_Haitham.xm "https://gitlab.com/hmeng80/AjPanel/-/raw/main/ajpanel_menu_Haitham.xml?ref_type=heads"
+wait
+wget -O /media/sda1/AJPanel_Backup/ajpanel_menu_Haitham.xm "https://gitlab.com/hmeng80/AjPanel/-/raw/main/ajpanel_menu_Haitham.xml?ref_type=heads"
+wait
+wget -O /media/dreambox-storage/AJPanel_Backup/ajpanel_menu_Haitham.xm "https://gitlab.com/hmeng80/AjPanel/-/raw/main/ajpanel_menu_Haitham.xml?ref_type=heads"
+wait
+wget -O /media/usb/ajpanel_menu_Haitham.xm "https://gitlab.com/hmeng80/AjPanel/-/raw/main/ajpanel_menu_Haitham.xml?ref_type=heads"
+exit 0
